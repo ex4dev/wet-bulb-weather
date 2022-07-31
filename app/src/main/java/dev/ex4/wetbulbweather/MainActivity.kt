2b1596608145027f4a9f5c1bd3164148067fc2aa
tylerswanson2 <31103658+tylerswanson2@users.noreply.github.com>
2022-07-31 18:13:57 -0400

    Add Settings activity

--- app/src/main/java/dev/ex4/wetbulbweather/MainActivity.kt
+++ app/src/main/java/dev/ex4/wetbulbweather/MainActivity.kt
@@ -1,5 +1,6 @@
 package dev.ex4.wetbulbweather
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -48,7 +49,10 @@ class MainActivity : AppCompatActivity() {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
